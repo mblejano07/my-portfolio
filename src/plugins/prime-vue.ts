ada607aad758052069f3032baefdeba8eb514049
jegramos <jegramos.pa@gmail.com>
2024-01-10 15:10:39 +0800

    :construction: Start

--- src/plugins/prime-vue.ts
+++ src/plugins/prime-vue.ts
@@ -1,12 +1,11 @@
 /** @see https://primevue.org/vite/ */
 
 import { vueApp } from '@/app.ts'
-import PrimeVue from 'primevue/config'
+import PrimeVue, { PrimeVueConfiguration } from 'primevue/config'
 import Wind from '@/assets/css/presets/wind'
-
 import ToastService from 'primevue/toastservice'
 
-const primeVueConfig = { unstyled: true, pt: Wind, ripple: true }
-vueApp.use(PrimeVue, primeVueConfig)
+const config: PrimeVueConfiguration = { unstyled: true, pt: Wind, ripple: true }
 
+vueApp.use(PrimeVue, config)
 vueApp.use(ToastService)
